17a30ecfdbe07dfdb6082e0063bf1ca6d1abb2c6
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-06 01:42:25 +0200

    Création de Reservation

---
+++ GestionHotel/Classes/Reservation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionHotel.Classes
+{
+    public class Reservation
+    {
+        private int codeReservation;
+        private int idClient;
+
+        public int CodeReservation { get => codeReservation; set => codeReservation = value; }
+        public int IdClient { get => idClient; set => idClient = value; }
+
+        enum Statut
+        {
+            valide,
+            annule
+        }
+    }
+}
