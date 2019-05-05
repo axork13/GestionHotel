3639c1416d69a0c358f3f666c0257c58a46ed54c
Jimmy Marmin <jimmy.marmin@gmail.com>
2019-05-06 01:37:54 +0200

    Création du client

---
+++ GestionHotel/Classes/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionHotel.Classes
+{
+    public class Client
+    {
+        private int idClient;
+        private string nomClient;
+        private string adresseClient;
+        private string telClient;
+
+        public Client(int idClient, string nomClient, string adresseClient, string telClient)
+        {
+            this.IdClient = idClient;
+            this.NomClient = nomClient;
+            this.AdresseClient = adresseClient;
+            this.TelClient = telClient;
+        }
+
+        public int IdClient { get => idClient; set => idClient = value; }
+        public string NomClient { get => nomClient; set => nomClient = value; }
+        public string AdresseClient { get => adresseClient; set => adresseClient = value; }
+        public string TelClient { get => telClient; set => telClient = value; }
+    }
+}
